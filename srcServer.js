04484c9449deb4e5576fa433651f802f5ed39357
psy <psy>
2017-02-04 16:21:42 -0500

    changed

--- srcServer.js
+++ srcServer.js
@@ -8,7 +8,6 @@ const port = 3000;
 const app = express();
 const compiler = webpack(config);
 
-
 //handle this way
 app.get("/", function(rep, res){
     res.sendFile(path.join(__dirname, "../src/index.html"));
